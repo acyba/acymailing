a5836fe30a906e2feeed0a654b5a01754f213eb0
Rémi Leclercq <leclercq.remi@gmail.com>
2020-10-01 10:40:12 +0200

    v6.16.2

--- back/views/campaigns/tmpl/edit_email_actions.php
+++ back/views/campaigns/tmpl/edit_email_actions.php
@@ -5,11 +5,11 @@
 	<div class="cell medium-auto grid-x text-right">
 		<div class="cell medium-auto"></div>
         <?php if (!empty($data['campaignID'])) { ?>
-			<button acym-data-before="acym_editorWysidMultilingual.storeCurrentValues(true);" data-task="save" data-step="listing" type="submit" class="cell button-secondary medium-shrink button medium-margin-bottom-0 margin-right-1 acy_button_submit">
+			<button <?php echo $data['before-save']; ?> data-task="save" data-step="listing" type="submit" class="cell button-secondary medium-shrink button medium-margin-bottom-0 margin-right-1 acy_button_submit">
                 <?php echo acym_translation('ACYM_SAVE_EXIT'); ?>
 			</button>
         <?php } ?>
-		<button acym-data-before="acym_editorWysidMultilingual.storeCurrentValues(true);" data-task="save" data-step="recipients" type="submit" class="cell medium-shrink button margin-bottom-0 acy_button_submit">
+		<button <?php echo $data['before-save']; ?> data-task="save" data-step="recipients" type="submit" class="cell medium-shrink button margin-bottom-0 acy_button_submit">
             <?php echo acym_translation('ACYM_SAVE_CONTINUE'); ?><i class="acymicon-chevron-right"></i>
 		</button>
 	</div>
--- gulp/gulp-synch.js
+++ gulp/gulp-synch.js
@@ -13,7 +13,7 @@ let multidest = require('gulp-multi-dest');
 let command = require('gulp-run-command').default;
 let babel = require('gulp-babel');
 
-let version = '6.16.1';
+let version = '6.16.2';
 
 let minifyJSOptions = {
     ext: {
