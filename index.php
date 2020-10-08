7bd7f3a0548cc0be2fd1ca032eec04678aea3303
Rémi Leclercq <leclercq.remi@gmail.com>
2020-10-08 15:38:43 +0200

    v6.16.4

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Description: Manage your contact lists and send newsletters from your site.
 Author: AcyMailing Newsletter Team
 Author URI: https://www.acyba.com
 License: GPLv3
-Version: 6.16.3
+Version: 6.16.4
 Text Domain: acymailing
 Domain Path: /language
 */
