e8691eef2a749555442c7fa9de5cf050626ccde2
Jérémy Dziergwa <jeremy.dziergwa@jetpulp.fr>
2026-02-26 17:41:54 +0100

    remove editor files

--- media/assets/js/blockEditor/editor-sample/node_modules/@mui/material/styles/variants.d.ts
+++
@@ -1,9 +0,0 @@
-import { Interpolation } from '@mui/system';
-import { ComponentsPropsList } from './props';
-
-export type ComponentsVariants<Theme = unknown> = {
-  [Name in keyof ComponentsPropsList]?: Array<{
-    props: Partial<ComponentsPropsList[Name]>;
-    style: Interpolation<{ theme: Theme }>;
-  }>;
-};
